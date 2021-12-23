1b859862407933e4ae75ca5d9517fddc84b51772
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-12-24 02:27:48 +0400

    About, Service, Interest models added.

--- about/models.py
+++ about/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class About(models.Model):
+    name = models.CharField(max_length=63, null=True, blank=True)
+    address = models.CharField(max_length=63, null=True, blank=True)
+    study = models.CharField(max_length=63, null=True, blank=True)
+    degree = models.CharField(max_length=63, null=True, blank=True)
+    mail = models.EmailField(max_length=63, null=True, blank=True)
+    phone = models.CharField(max_length=63, null=True, blank=True)
+    
+    class Meta:
+        verbose_name = 'About'
+        verbose_name_plural = 'About'
+        
+    def __str__(self):
+        return self.name
+    
+
+class Service(models.Model):
+    service = models.CharField(max_length=127, null=True, blank=True)
+    
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
+        
+    def __str__(self):
+        return self.service
+    
+    
+class Interest(models.Model):
+    interest = models.CharField(max_length=127, null=True, blank=True)
+    
+    class Meta:
+        verbose_name = 'Interest'
+        verbose_name_plural = 'Interests'
+        
+    def __str__(self):
+        return self.interest
--- about/views.py
+++ about/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from . models import About, Interest, Service
 # Create your views here.
+
+def home(request):
+    about = About.objects.get(pk=1)
+    services = Service.objects.all()
+    interests = Interest.objects.all()
+    context = {
+        'about':about,
+        'services':services,
+        'interests':interests,
+    }
+    return render(request, "index.html", context)
