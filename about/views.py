3c76ba5e1969d11d8d5f2fa47db767489ece0372
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-09 18:26:30 +0400

    Experience view and template configuration

--- about/views.py
+++ about/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import About, Interest, Service, Education
+from . models import About, Interest, Service, Education, Experience
 # Create your views here.
 
 def home(request):
@@ -7,11 +7,13 @@ def home(request):
     services = Service.objects.all()
     interests = Interest.objects.all()
     educations = Education.objects.all()
+    experiences = Experience.objects.all()
     
     context = {
         'about':about,
         'services':services,
         'interests':interests,
         'educations':educations,
+        'experiences':experiences
     }
     return render(request, "index.html", context)
