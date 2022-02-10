adb7e8f13c8e52173a6f5c249ac70dae596775a7
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-10 12:15:28 +0400

    Contact view created

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import redirect, render
+from . forms import ContactForm
+from contact.utils.validators import ValidationError
+from django.contrib import messages
 
-# Create your views here.
+
+def contact(request):
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.INFO, 'Successfuly registration!')
+            return redirect('/contact')
+        else:
+            messages.add_message(request, messages.INFO, 'Your mail is not valid!')
+        
+    return render(request, 'contact.html', {'form': form})
