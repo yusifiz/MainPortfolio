d864144522384b5c9496a0b6bb1bcb61e1536987
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-18 16:59:28 +0400

    deploy

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lq73$ly-4ys#(zctj-%8of*(y5y@n)r^2um31wch8+zf(^g$zv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://yusifiz.herokuapp.com',
                  'localhost',
