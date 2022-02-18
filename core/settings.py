0111185f08c6086fe83ab33e8194420cace089bc
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-02-18 04:32:30 +0400

    deploy

--- core/settings.py
+++ core/settings.py
@@ -84,21 +84,6 @@ DATABASES = {
     }
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
 
 
 # Password validation
